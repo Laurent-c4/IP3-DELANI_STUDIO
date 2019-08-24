8f2a3cfd3d36c648858e5f83d9d9ed9db2a29e40
Laurent Juma <lcbillax.lj@gmail.com>
2019-08-24 23:39:56 +0300

    Add hover effect to elements of the 2 rows of Portfolio Section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+//USER INTERFACE LOGIC
+
+// Toggle function for 'What We Do' section
   $("#design_icon").click(function() {
     $("#design_icon").toggle();
     $("#design_description").toggle();
@@ -24,4 +28,61 @@ $(document).ready(function() {
     $("#product_description").toggle();
   });
 
+//Hover Effect for Portfolio section
+  $("#img_1").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_1").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_1").fadeTo("slow", 0);
+  });
+  $("#img_2").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_2").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_2").fadeTo("slow", 0);
+  });
+  $("#img_3").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_3").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_3").fadeTo("slow", 0);
+  });
+  $("#img_4").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_4").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_4").fadeTo("slow", 0);
+  });
+  $("#img_5").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_5").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_5").fadeTo("slow", 0);
+  });
+  $("#img_6").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_6").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_6").fadeTo("slow", 0);
+  });
+  $("#img_7").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_7").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_7").fadeTo("slow", 0);
+  });
+  $("#img_8").hover(function() {
+    $(this).fadeTo("slow", 0.5);
+    $("#project_8").fadeTo("slow", 1);
+  }, function() {
+    $(this).fadeTo("slow", 1);
+    $("#project_8").fadeTo("slow", 0);
+  });
 });
